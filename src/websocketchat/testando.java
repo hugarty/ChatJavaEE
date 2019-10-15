0cfebd6d7ce92ce5a9a1e715c181dec897489f28
hugarty <hp-ca@hotmail.com>
2019-10-15 19:48:35 -0300

    Criando comunicação entre cliente e servidor

--- src/websocketchat/testando.java
+++ src/websocketchat/testando.java
@@ -1,12 +1,49 @@
 package websocketchat;
 
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/ooooi")
+
+@ServerEndpoint("/chat")
 public class testando {
+	
 	@OnOpen
-	public void entrouAlguem () {
-		System.out.println("eae");
+	public void entrouAlguem(Session session) {
+		System.out.println("OI");
+		try {
+			session.getBasicRemote().sendText("OI");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@OnClose
+	public void saiuAlguem() {
+		System.out.println("Saiu ");
+	}
+	
+	@OnMessage
+	public void recebeuMensagem(String message, Session session) {
+		try {
+			session.getBasicRemote().sendText(message);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
